5ec9d9d9232e4ce9d75d1b2c22c42e3608d64ae0
Pbravim <bravim.pedro@gmail.com>
2024-11-05 09:30:54 -0300

    12

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
+// main.ts
 import app from "./app";
-// import sequelize from "./config/sequelize";
+import https from 'https';
+import fs from 'fs';
+import path from 'path';
 
-// sequelize.sync({force: true});
 
-app.start(3000);
+const sslOptions = {
+    key: fs.readFileSync(path.join ('certs', 'key.pem')),
+    cert: fs.readFileSync(path.join( 'certs', 'cert.pem'))
+};
+
+// Certifique-se de que 'app.app' é a instância correta do Express
+https.createServer(sslOptions, app.app.getExpressApp()).listen(3000, () => {
+  console.log('Servidor HTTPS rodando na porta 3000');
+});
